c442b9f700bb77b4635907f3c30f07d3dfb4bc63
16Shadows <egor.0.00@mail.ru>
2023-06-26 02:39:37 +0500

    Added extra method for assert.

---
+++ AssertEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSToolbox
+{
+    public static class AssertEx
+    {
+        public static void IsArgumentNull<T>(T argument, string name)
+        {
+            if (argument == null)
+                throw new ArgumentNullException(name);
+        }
+    }
+}
