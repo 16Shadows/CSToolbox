445bde562c8dcb9faf8d93d064488b5ca7d5de5e
16Shadows <egor.0.00@mail.ru>
2024-02-17 14:10:05 +0500

    Added ListExtensions to convert a list to a string using custom separator.

--- CSToolbox/Extensions/ListExtensions.cs
+++ CSToolbox/Extensions/ListExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace CSToolbox.Extensions
 {
@@ -32,5 +34,41 @@ namespace CSToolbox.Extensions
             }
             return -1;
         }
+
+        /// <summary>
+        /// Generates a string from a list adding separator in-between. Uses <see cref="object.ToString"/> to convert items within the list into strings.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the list.</typeparam>
+        /// <param name="list">The list to convert.</param>
+        /// <param name="separator">The character to use as separator between string representation of objects in the list.</param>
+        /// <returns></returns>
+        public static string ToString<T>(this IList<T> list, char separator) => ToString(list, separator.ToString());
+
+        /// <summary>
+        /// Generates a string from a list adding separator in-between. Uses <see cref="object.ToString"/> to convert items within the list into strings.
+        /// </summary>
+        /// <typeparam name="T">The type of items in the list.</typeparam>
+        /// <param name="list">The list to convert.</param>
+        /// <param name="separator">The string to use as separator between string representation of objects in the list.</param>
+        /// <returns></returns>
+		public static string ToString<T>(this IList<T> list, string separator)
+		{
+            ArgumentNullException.ThrowIfNull(list);
+            ArgumentNullException.ThrowIfNull(separator);
+
+			if (list.Count == 0)
+				return string.Empty;
+
+			StringBuilder sb = new StringBuilder(list.Count * 10);
+
+			sb.Append(list[0]);
+			for (int i = 1;  i < list.Count; i++)
+			{
+				sb.Append(separator);
+				sb.Append(list[i]);
+			}
+
+			return sb.ToString();
+		}
     }
 }
