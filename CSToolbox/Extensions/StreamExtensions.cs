ed06bcf40a656e318f1c9091b189677560dd1562
16Shadows <egor.0.00@mail.ru>
2024-02-23 18:40:16 +0500

    Added StreamExtension Write(this Stream s, Stream stream) which writes one stream into another.

---
+++ CSToolbox/Extensions/StreamExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace CSToolbox.Extensions
+{
+	public static class StreamExtensions
+	{
+		public static void Write(this Stream thisStream, Stream stream)
+		{
+			const int chunkSize = 8*1024;
+
+			//Write in chunks of 8KB
+			byte[] buffer = new byte[chunkSize];
+
+			int readBytes;
+			while((readBytes = stream.Read(buffer, 0, chunkSize)) > 0)
+				thisStream.Write(buffer, 0, readBytes);
+		}
+	}
+}
