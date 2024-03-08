7b124286629b9fcee5fe6fbc7a18021e96501daa
16Shadows <egor.0.00@mail.ru>
2024-03-08 19:48:06 +0500

    Added TempFileStreamm which opens a stream to a temporary file which will be deleted when the stream is closed.

---
+++ CSToolbox/IO/TempFileStream.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace CSToolbox.IO
+{
+	/// <summary>
+	/// A simple implementation which creates a temporary file when created and destroys it when disposed of.
+	/// </summary>
+	public class TempFileStream : FileStream
+	{
+		public TempFileStream() : base(Path.GetTempFileName(), FileMode.Open, FileAccess.ReadWrite) {}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			File.Delete(Name);
+		}
+	}
+}
