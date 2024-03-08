bf099ee1b9f07f9084853e64aae25b492df7ecb0
16Shadows <egor.0.00@mail.ru>
2024-03-08 19:47:34 +0500

    Added ReadOnlyCollection extension RandomElement()

---
+++ CSToolbox/Extensions/IReadOnlyCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSToolbox.Extensions
+{
+	public static class IReadOnlyCollectionExtensions
+	{
+		public static T RandomElement<T>(this IReadOnlyCollection<T> collection)
+			=>collection.Skip(System.Random.Shared.Next(0, collection.Count)).First();
+	}
+}
