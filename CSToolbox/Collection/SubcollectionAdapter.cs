f851c915b1b0f6bef6e6f377579ebf712aa32f9f
16Shadows <egor.0.00@mail.ru>
2024-03-23 20:56:46 +0500

    Added SubcollectionAdapter and SublistAdapter which represent mutable views of collections.

---
+++ CSToolbox/Collection/SubcollectionAdapter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSToolbox.Collection
+{
+	/// <summary>
+	/// Implements a 'view' of an adapted collection. 
+	/// This collection acts as an adapter for a collection of <typeparamref name="TFrom"/> presenting only items of type <typeparamref name="TTo"/>.
+	/// All changes made to this collection are reflected in the adapted collection and vice versa.
+	/// </summary>
+	/// <typeparam name="TFrom">The type of items in the adapted collection.</typeparam>
+	/// <typeparam name="TTo">The type of items to present in this view.</typeparam>
+	public class SubcollectionAdapter<TFrom, TTo> : ICollection<TTo>, IReadOnlyCollection<TTo> where TTo : TFrom
+	{
+		protected readonly ICollection<TFrom> _AdaptedCollection;
+
+		/// <summary>
+		/// Creates an instance of <see cref="SubcollectionAdapter{TFrom, TTo}"/> which adapts <paramref name="adaptedCollection"/>.
+		/// </summary>
+		/// <param name="adaptedCollection">The collection to adapt.</param>
+		public SubcollectionAdapter(ICollection<TFrom> adaptedCollection)
+		{
+			ArgumentNullException.ThrowIfNull(adaptedCollection);
+
+			_AdaptedCollection = adaptedCollection;
+		}
+
+		/// <summary>
+		/// Computes number of items of type <typeparamref name="TTo"/> in the adapted collection.
+		/// Iterates the adapted collection.
+		/// </summary>
+		public int Count => _AdaptedCollection.Count(x => x is TTo);
+
+		/// <summary>
+		/// Returns the same value as <see cref="IsReadOnly"/> in adapted collection.
+		/// </summary>
+		public bool IsReadOnly => _AdaptedCollection.IsReadOnly;
+
+		/// <summary>
+		/// Adds an item into the adapted collection.
+		/// </summary>
+		/// <param name="item">The item to add.</param>
+		public virtual void Add(TTo item)
+		{
+			_AdaptedCollection.Add(item);
+		}
+
+		/// <summary>
+		/// Removes all items of type <typeparamref name="TTo"/> from the adapted collection.
+		/// Standard implementation causes an enumeration of the adapted collection and allocates an array of size <see cref="Count"/>.
+		/// </summary>
+		public virtual void Clear()
+		{
+			TTo[] items = this.ToArray();
+			foreach (TTo item in items)
+				_AdaptedCollection.Remove(item);
+		}
+
+		/// <summary>
+		/// Checks whether the adapted collection contains a given item of type <typeparamref name="TTo"/>.
+		/// </summary>
+		/// <param name="item">The item to check for.</param>
+		/// <returns>True if the collection contains the item, false otherwise.</returns>
+		public virtual bool Contains(TTo item)
+		{
+			return _AdaptedCollection.Contains(item);
+		}
+
+		/// <summary>
+		/// Copies only items of type <typeparamref name="TTo"/> from the adapted collection.
+		/// </summary>
+		/// <param name="array">The array to write items to.</param>
+		/// <param name="arrayIndex">The index to start from.</param>
+		public virtual void CopyTo(TTo[] array, int arrayIndex)
+		{
+			foreach(TTo item in this)
+				array[arrayIndex++] = item;
+		}
+
+		/// <summary>
+		/// Returns an enumerator of items of type <typeparamref name="TTo"/> in the adapted collection.
+		/// </summary>
+		/// <returns>The enumerator.</returns>
+		public virtual IEnumerator<TTo> GetEnumerator()
+		{
+			return _AdaptedCollection.Where(x => x is TTo).Cast<TTo>().GetEnumerator();
+		}
+
+		/// <summary>
+		/// Removes an item of type <typeparamref name="TTo"/> from the adapted collection.
+		/// </summary>
+		/// <param name="item">The item to remove.</param>
+		/// <returns>True if the item was removed, false otherwise.</returns>
+		public virtual bool Remove(TTo item)
+		{
+			return _AdaptedCollection.Remove(item);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ CSToolbox/Collection/SublistAdapter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSToolbox.Collection
+{
+	/// <summary>
+	/// Implements a 'view' of an adapted list. 
+	/// This list acts as an adapter for a list of <typeparamref name="TFrom"/> presenting only items of type <typeparamref name="TTo"/>.
+	/// All changes made to this list are reflected in the adapted list and vice versa.
+	/// </summary>
+	/// <typeparam name="TFrom">The type of items in the adapted list.</typeparam>
+	/// <typeparam name="TTo">The type of items to present in this view.</typeparam>
+	public class SublistAdapter<TFrom, TTo> : SubcollectionAdapter<TFrom, TTo>, IList<TTo>, IReadOnlyList<TTo> where TTo : TFrom
+	{
+		protected readonly IList<TFrom> _AdaptedList;
+
+		/// <summary>
+		/// Creates an instance of <see cref="SublistAdapter{TFrom, TTo}"/> which adapts <paramref name="adaptedCollection"/>.
+		/// </summary>
+		/// <param name="adaptedCollection">The list to adapt.</param>
+		public SublistAdapter(IList<TFrom> adaptedCollection) : base(adaptedCollection)
+		{
+			ArgumentNullException.ThrowIfNull(adaptedCollection);
+
+			_AdaptedList = adaptedCollection;
+		}
+
+		/// <summary>
+		/// Convert an index in this view into an index in the adapted list.
+		/// </summary>
+		/// <param name="index">The index to adapt.</param>
+		/// <returns>The index in the adapted collection of the item located at <paramref name="index"/> in this view.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">Is thrown if the <paramref name="index"/> is less than 0 or greated than or equals to <see cref="SubcollectionAdapter{TFrom, TTo}.Count"/></exception>
+		protected int GetAdaptedIndex(int index)
+		{
+			if (index < 0)
+					throw new ArgumentOutOfRangeException(nameof(index));
+
+			int i;
+			for (i = 0; i < _AdaptedList.Count; i++)
+			{
+				if (_AdaptedList[i] is not TTo)
+					continue;
+					
+				index--;
+				if (index < 0)
+					break;
+			}
+
+			if (index >= 0)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			return i;
+		}
+
+		/// <summary>
+		/// An indexer into this view. Each access causes a partial iteration of the adapted list to reach the desired index.
+		/// </summary>
+		/// <param name="index">The index of an item in this view.</param>
+		/// <returns></returns>
+		public TTo this[int index]
+		{
+			get => (TTo)_AdaptedList[GetAdaptedIndex(index)]!;
+			set => _AdaptedList[GetAdaptedIndex(index)] = value;
+		}
+
+		/// <summary>
+		/// Finds an index of an item in this view.
+		/// </summary>
+		/// <param name="item">The item to find the index for.</param>
+		/// <returns>The index of the item, or -1 if the item wasn't found.</returns>
+		public int IndexOf(TTo item)
+		{
+			int internalIndex = 0;
+			for (int i = 0; i < _AdaptedList.Count; i++)
+			{
+				if (_AdaptedList[i] is not TTo)
+					continue;
+				else if (EqualityComparer<TTo>.Default.Equals(item, (TTo?)_AdaptedList[i]))
+					return internalIndex;
+
+				internalIndex++;
+			}
+			return -1;
+		}
+
+		/// <summary>
+		/// Inserts an item into the adapted collection so that it would be at <paramref name="index"/> in this view.
+		/// No guarantees are made regarding the actual index in the adapted collection.
+		/// This method causes a partial iteration of the adapted collection.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <param name="item"></param>
+		public void Insert(int index, TTo item)
+		{
+			_AdaptedList.Insert(GetAdaptedIndex(index), item);
+		}
+
+		/// <summary>
+		/// Removes an item from in adapted collection located at <paramref name="index"/> in this view.
+		/// </summary>
+		/// <param name="index">The index of an item in this view.</param>
+		public void RemoveAt(int index)
+		{
+			_AdaptedList.RemoveAt(GetAdaptedIndex(index));
+		}
+
+		/// <summary>
+		/// Removes all items of type <typeparamref name="TTo"/> from the adapted collection.
+		/// This implementation causes an enumeration of the adapted list.
+		/// </summary>
+		public override void Clear()
+		{
+			for (int i = 0; i < _AdaptedList.Count;)
+			{
+				if (_AdaptedList[i] is TTo)
+					_AdaptedList.RemoveAt(i);
+				else
+					i++;
+			}
+		}
+	}
+}
